2f6a2636439f9a9ec72eeb853b1f3dc0f7774581
Anirudh Iyer <ani.iyer@protonmail.com>
2025-03-14 18:45:46 +0530

    nit

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 export default function ResumePage() {
-  const router = useRouter();
-
   useEffect(() => {
     // Redirect to the PDF file
     window.location.href = "/resume.pdf";
